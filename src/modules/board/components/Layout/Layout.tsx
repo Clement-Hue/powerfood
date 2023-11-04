aad7927be3d861900ec4f57ed5d59c20a475a30a
Clément Hue <clementhue@hotmail.fr>
2023-11-04 14:17:59 +0100

    Init project

---
+++ src/modules/board/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Input} from "@shares"
+
+const Layout = () => {
+    return (
+        <div>
+            <Input label="Rechercher un aliment"/>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/shares/input/Input.tsx
@@ -0,0 +1,17 @@
+import React, {useId} from 'react';
+import classes from "./Input.module.scss"
+
+const Input: React.FC<Props> = ({label, ...props}) => {
+    const id = useId();
+    return (
+        <>
+            <label htmlFor={id}>{label}></label>
+            <input {...props} id={id}/>
+        </>
+    )
+};
+
+type Props = Omit<React.HTMLAttributes<HTMLInputElement>, "id"> & {
+    label?: React.ReactNode
+}
+export default Input;
