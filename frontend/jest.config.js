9a5f93f8805fd201c28767929005a3ed87da9fa9
Clément Hue <clementhue@hotmail.fr>
2023-11-06 16:14:40 +0100

    Test working

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,7 +10,7 @@ export default {
   modulePaths: [tsConfig.compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
   moduleNameMapper: {
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file.mock.js",
-      "\\.(css|less|scss)$": "<rootDir>/__mocks__/file.mock.js",
+      "\\.(css|less|scss)$": "identity-obj-proxy",
     ...pathsToModuleNameMapper(tsConfig.compilerOptions.paths)},
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
 };
--- frontend/src/modules/board/board.test.tsx
+++ frontend/src/modules/board/board.test.tsx
@@ -1,8 +1,25 @@
 import {render} from "@testing";
 import {Board} from "../index.ts";
+import {ServicesProvider} from "@providers";
 
+const TestComponent = () => {
+    return (
+        <ServicesProvider overrides={{
+            apiService: {
+                getFoods() {
+                    return []
+                },
+                getNutrients() {
+                    return []
+                }
+            }
+        }}>
+            <Board/>
+        </ServicesProvider>
+    )
+}
 describe("Board module", () => {
     it("should render board", () => {
-        render(<Board/>)
+        render( <TestComponent/>)
     })
 })
