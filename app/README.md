dd1eb3e9798e9eb3f5f171543e80d87fba69e7e9
Clément Hue <clementhue@hotmail.fr>
2023-11-25 20:24:03 +0100

    Use class to style the chart

--- app/README.md
+++ app/README.md
@@ -1,4 +1,2 @@
-- Remove test Summary
-- Create unit test for the store selectMicros
 - Create test for the visiblity of tooltip on mouse over
 - Refacto NutritionGraph
--- app/src/modules/board/components/NutrientGraph/NutrientGraph.tsx
+++ app/src/modules/board/components/NutrientGraph/NutrientGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useId, useMemo, useRef } from 'react';
+import React, { useCallback, useEffect, useId, useRef } from 'react';
 import * as d3 from "d3";
 import classes from "./NutrientGraph.module.scss"
 import { useAppSelector } from '@hooks';
@@ -7,11 +7,6 @@ import clsx from 'clsx';
 
 const NutrientGraph: React.FC<Props> = ({ nutrientId, dayName, open: show = false}) => {
     const graphContainerRef = useRef(null);
-    const chartProps = useMemo(() => ({
-        width: 500, height: 300, margin: {
-            top: 30, right: 30, bottom: 130, left:100
-        }
-    }), []);
     const micros = useAppSelector((state) => daySelectors.selectMicros(state, dayName));
     const titleId = useId();
     const nutrientInfo = micros?.find((m) => m.id === nutrientId);
@@ -20,47 +15,42 @@ const NutrientGraph: React.FC<Props> = ({ nutrientId, dayName, open: show = fals
         if (!nutrientInfo) {
             return;
         }
-        const fontSize = "1.2rem";
-        const container = d3.select(graphContainerRef.current)
-            .append("svg")
-            .attr("width", chartProps.width + chartProps.margin.left + chartProps.margin.right)
-            .attr("height", chartProps.height + chartProps.margin.top + chartProps.margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${chartProps.margin.left}, ${chartProps.margin.top})`)
+        const container = d3.select(graphContainerRef.current).append("svg")
+        .attr("class", classes["chart__svg"])
+        .append("g")
+        .attr("class", classes["chart__container"])
 
         const xAxis = d3.scaleBand()
-            .range([0, chartProps.width])
-            .domain(nutrientInfo.foods.sort((a, b) => d3.descending(a.amount, b.amount)).map((f) => f.food.name))
-            .padding(0.2)
+        .range([0, parseInt(classes.width, 10)])
+        .domain(nutrientInfo.foods.sort((a, b) => d3.descending(a.amount, b.amount)).map((f) => f.food.name))
+        .padding(0.2)
 
         const yAxis = d3.scaleLinear()
-            .range([chartProps.height, 0])
-            .domain([0, d3.max(nutrientInfo.foods.map((f) => f.amount)) ?? 0])
+        .range([parseInt(classes.height, 10), 0])
+        .domain([0, d3.max(nutrientInfo.foods.map((f) => f.amount)) ?? 0])
 
         container.append("g")
-            .attr("transform", `translate(0, ${chartProps.height})`)
+            .attr("class", classes["chart__x-container"])
             .call(d3.axisBottom(xAxis))
             .selectAll("text")
-            .attr("transform", "translate(-10,0) rotate(-45)")
-            .style("text-anchor", "end")
-            .style("font-size", fontSize)
+            .attr("class", classes["chart__x-label"])
+
 
         container.append("g")
             .call(d3.axisLeft(yAxis))
             .selectAll("text")
-            .style("text-anchor", "end")
-            .style("font-size", fontSize)
+            .attr("class", classes["chart__y-label"])
 
         const bar = container.selectAll("bar")
-            .data(nutrientInfo.foods)
-            .enter()
-            .append("g")
+        .data(nutrientInfo.foods)
+        .enter()
+        .append("g")
 
         bar.append("rect")
             .attr("x", (d) => xAxis(d.food.name) ?? 0)
             .attr("y", (d) => yAxis(d.amount))
             .attr("width", xAxis.bandwidth())
-            .attr("height", (d) => chartProps.height - yAxis(d.amount))
+            .attr("height", (d) => parseInt(classes.height, 10) - yAxis(d.amount))
             .attr("fill", "var(--dark-primary)")
 
         bar.append("text")
@@ -68,10 +58,10 @@ const NutrientGraph: React.FC<Props> = ({ nutrientId, dayName, open: show = fals
             .attr("x", (d) => (xAxis(d.food.name) ?? 0)  + xAxis.bandwidth() / 2)
             .attr("y", (d) => yAxis(d.amount) - 5)
             .attr("text-anchor", "middle")
-    }, [chartProps, nutrientInfo])
+    }, [nutrientInfo])
 
     useEffect(() => {
-       makeGraph() 
+        makeGraph() 
         return () => {
             d3.select(graphContainerRef.current).selectAll("*").remove()
         }
